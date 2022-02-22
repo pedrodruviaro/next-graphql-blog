c7bcdeefb7244dd9d8bc7d9b09dec8b41a943adf
pedrodruviaro <82953655+pedrodruviaro@users.noreply.github.com>
2022-02-22 20:55:52 -0300

    code setup -> components, layout, global scss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
 
 const posts = [
   {
@@ -23,15 +24,15 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post) => (
-            <div key={post.title}>
-              {post.title}
-              {post.excerpt}
-            </div>
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
 
         <div className="col-span-1 lg:col-span-4">
-          <div className="relative top-8 lg:sticky">30:44</div>
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
